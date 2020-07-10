87f44ee6f24264635e166a0e374269d3aabbb448
devin.bob.shieh@gmail.com <kgfc64579A>
2020-07-10 17:33:57 +0800

    删除询盘、主题、登录、设置、LOST等页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/passport/login.vue'
 import layout from '../components/layout/content.vue'
 import i18n from '../plugins/i18n/base'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/passport',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/passport',
-    component: layout,
-    meta: {
-      title: i18n.t('passport.login.title'),
-      requireAuth: false,
-      hideCrumb: true,
-      fullScreen: true
-    },
-    children: [
-      {
-        path: '',
-        name: 'passport-login',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: true,
-          fullScreen: true
-        },
-        component: () => import('../views/passport/login.vue')
-      }
-    ]
-  },
-  {
-    path: '/settings',
-    component: layout,
-    children: [
-      {
-        path: '',
-        name: 'settings-language',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/settings/language')
-      }
-    ]
-  },
   {
     path: '/:shopCode',
     component: layout,
@@ -73,90 +26,8 @@ const routes = [
           fullScreen: true
         },
         component: () => import('../views/store/design/index.vue')
-      },
-      {
-        path: 'themes',
-        name: 'themes',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/themes/index.vue')
-      },
-      {
-        path: 'enquiry/form',
-        name: 'enquiry-form',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/enquiry/form/index')
-      },
-      {
-        path: 'enquiry/form/add',
-        name: 'enquiry-form-add',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/enquiry/form/update')
-      },
-      {
-        path: 'enquiry/form/update/:id',
-        name: 'enquiry-form-update',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/enquiry/form/update')
-      },
-      {
-        path: 'enquiry/email',
-        name: 'enquiry-email',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/enquiry/email/index')
-      },
-      {
-        path: 'enquiry/inbox',
-        name: 'enquiry-inbox',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/enquiry/inbox/index')
-      },
-      {
-        path: 'enquiry/inbox/update/:id',
-        name: 'enquiry-inbox-update',
-        meta: {
-          title: i18n.t('passport.login.title'),
-          requireAuth: false,
-          hideCrumb: false,
-          fullScreen: false
-        },
-        component: () => import('../views/store/enquiry/inbox/update')
       }
     ]
-  },
-  {
-    path: '*',
-    name: '404',
-    component: () => import('../views/store/pages/lost.vue')
   }
 ]
 
